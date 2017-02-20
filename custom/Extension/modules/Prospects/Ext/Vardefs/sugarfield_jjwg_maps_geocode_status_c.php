2ebd07eb423f85d998d3e18cc76345448f5c0669
Steel Ricciotti <Steel Ricciotti>
2017-02-20 13:32:35 -0500

    Initial commit

---
+++ custom/Extension/modules/Prospects/Ext/Vardefs/sugarfield_jjwg_maps_geocode_status_c.php
@@ -0,0 +1,5 @@
+<?php
+ // created: 2017-02-06 14:43:59
+$dictionary['Prospect']['fields']['jjwg_maps_geocode_status_c']['inline_edit']=1;
+
+ ?>
---
+++ custom/include/tabConfig.php
@@ -0,0 +1,57 @@
+<?php
+// created: 2017-02-19 10:41:02
+$GLOBALS['tabStructure'] = array (
+  'LBL_TABGROUP_ACTIVITIES' => 
+  array (
+    'label' => 'LBL_TABGROUP_ACTIVITIES',
+    'modules' => 
+    array (
+      0 => 'Home',
+      1 => 'Accounts',
+      2 => 'Calls',
+      3 => 'Meetings',
+      4 => 'Notes',
+      5 => 'Emails',
+    ),
+  ),
+  'LBL_TABGROUP_COLLABORATION' => 
+  array (
+    'label' => 'LBL_TABGROUP_COLLABORATION',
+    'modules' => 
+    array (
+      0 => 'Home',
+      1 => 'Leads',
+      2 => 'Opportunities',
+      3 => 'Prospects',
+      4 => 'ProspectLists',
+      5 => 'jjwg_Maps',
+    ),
+  ),
+  'LBL_GROUPTAB2_1486803356' => 
+  array (
+    'label' => 'LBL_GROUPTAB2_1486803356',
+    'modules' => 
+    array (
+      0 => 'Documents',
+      1 => 'AOS_Products',
+    ),
+  ),
+  'LBL_GROUPTAB3_1486803356' => 
+  array (
+    'label' => 'LBL_GROUPTAB3_1486803356',
+    'modules' => 
+    array (
+      0 => 'Project',
+      1 => 'Accounts',
+    ),
+  ),
+  'LBL_GROUPTAB5_1486803356' => 
+  array (
+    'label' => 'LBL_GROUPTAB5_1486803356',
+    'modules' => 
+    array (
+      0 => 'jjwg_Maps',
+      1 => 'Accounts',
+    ),
+  ),
+);
